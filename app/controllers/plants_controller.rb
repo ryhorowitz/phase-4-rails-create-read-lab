d6bf997307c1ec0195876e2a0e77188fbdf2677d
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-05-28 17:50:03 -0400

    complete

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,25 @@
+class PlantsController < ApplicationController
+  wrap_parameters format: []
+
+  def index
+    plants = Plant.all
+    render json: plants, status: :ok
+  end
+
+  def show
+    # byebug
+    plant = Plant.find_by(id: params[:id])
+    render json: plant, status: :ok
+  end
+
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  private
+
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
+end
